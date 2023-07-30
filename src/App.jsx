70ec467376f4d6da9de39c2499568486bb9f62fb
adityach0uhan <m4with6x@gmail.com>
2023-07-31 00:07:13 +0530

    Styling almost done

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='Main-Container'>
+
+
+        <div className='Number-Box'> <h4>10</h4></div>
+        <div className='btn-container'>
+        <button className='btn decrement-btn'>-</button>
+        <button className='btn increment-btn'>+</button>
+        </div>
+      </div>
+
+    
+   
+    
+    </>
+  );
+}
+
+export default App;
